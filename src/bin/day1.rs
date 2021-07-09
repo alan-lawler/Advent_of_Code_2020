0d2d43ab53c108b5fc5c390ba8b3a429b3d7b022
Alan L <alan.lawler@outlook.com>
2021-07-08 23:54:45 -0400

    Added part two

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -18,17 +18,37 @@ fn main() {
     //     list.push(n);
     // }
 
-    let result = read(list);
-    println!("{}", result.unwrap())
+    let part1 = read(&list);
+    let part2 = read_part_two(&list);
+    println!("Part 1: {}", part1.unwrap());
+    println!("Part 2: {}", part2.unwrap());
 }
 
-fn read(list: Vec<u32>) -> Option<u32> {
+fn read(list: &Vec<u32>) -> Option<u32> {
+    let list = list.clone();
     for number1 in &list {
         for number2 in &list {
             if number1 + number2 == 2020 {
                 println!("Number 1: {}", number1);
                 println!("Number 2: {}", number2);
                 return Some(number1 * number2)
+            }
+        }
+    }
+    None
+}
+
+fn read_part_two(list: &Vec<u32>) -> Option<u32> {
+    let list = list.clone();
+    for number1 in &list {
+        for number2 in &list {
+            for number3 in &list {
+                if number1 + number2 + number3 == 2020 {
+                    println!("Number 1: {}", number1);
+                    println!("Number 2: {}", number2);
+                    println!("Number 2: {}", number3);
+                    return Some(number1 * number2 * number3)
+                }
             }
         }
     }
