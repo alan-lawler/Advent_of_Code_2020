183d2b0d7a297a957e29fd913e6f1b13383b54a9
alan <alan.lawler@outlook.com>
2021-10-06 00:13:11 -0400

    finished part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -6,25 +6,39 @@ use regex::Regex;
 fn main() {
     let input_path = "../../files/day2_input.txt";
     let file = File::open(&input_path).unwrap();
-    let reader = BufReader::new(file);
-    let re = Regex::new(r"^(?P<range1>\d+)-(?P<range2>\d+)\s(?P<key>\w):\s(?P<pass>\w+)").unwrap();
+    let reader = BufReader::new(&file);
+    let (p1, p2) = part_one(reader);
+    println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
+}
+
+fn part_one(reader: BufReader<&File>) -> (i32, i32) {
+    let re = Regex::new(r"^(?P<start>\d+)-(?P<end>\d+)\s(?P<key>\w):\s(?P<pass>\w+)").unwrap();
     let mut count = 0;
+    let mut count_p2 = 0;
 
     for line in reader.lines() {
         let line = line.unwrap();
         let caps = re.captures(&line).unwrap();
-        let start = caps.name("range1").unwrap().as_str().parse::<u32>().unwrap();
-        let end = caps.name("range2").unwrap().as_str().parse::<u32>().unwrap();
+        let start = caps.name("start").unwrap().as_str().parse::<usize>().unwrap();
+        let end = caps.name("end").unwrap().as_str().parse::<usize>().unwrap();
         let key = caps.name("key").unwrap().as_str();
         let pass = caps.name("pass").unwrap().as_str();
 
         if pass.contains(key) {
-            let c = pass.matches(key).count() as u32;
+            let c = pass.matches(key).count() as usize;
             if c >= start && c <= end {
-                // println!("{}", line);
                 count += 1;
+            }
+
+            let password: Vec<char> = pass.chars().collect();
+            if password[start - 1].to_owned().to_string() == key && password[end - 1].to_owned().to_string() != key{
+                count_p2 += 1;
+            }
+            if password[start - 1].to_owned().to_string() != key && password[end - 1].to_owned().to_string() == key {
+                count_p2 += 1;
             }
         }
     }
-    println!("Number of results: {}", count);
+    (count, count_p2)
 }
